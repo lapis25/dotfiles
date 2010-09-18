0e7df711b5c373b3af57fcdcb59fb3e1eab2339a
lapis25 <lapis25@gmail.com>
2010-09-19 01:07:46 +0900

    終了時にmigemoの問い合わせをされないように修正

--- .emacs.d/elisp/migemo.el
+++ .emacs.d/elisp/migemo.el
@@ -208,10 +208,10 @@
 			     (list "-r" migemo-regex-dictionary))
 			   (list "-d" migemo-dictionary)))))
 	(setq migemo-buffer (get-buffer-create " *migemo*"))
-	(setq migemo-process (migemo-start-process 
+	(setq migemo-process (migemo-start-process
 			      "migemo" migemo-buffer migemo-command options))
 	;; (process-kill-without-query migemo-process)
-	(process-query-on-exit-flag migemo-process)
+	(set-process-query-on-exit-flag migemo-process nil)
 	t)))
 
 (defun migemo-replace-in-string (string from to)
