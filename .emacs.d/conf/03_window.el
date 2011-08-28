b4962dd7d69d6884a28717d78468490cb2ebeb11
lapis25 <lapis25@gmail.com>
2011-08-29 00:55:36 +0900

    scroll-barの非表示と現在行の反転を追加

--- .emacs.d/conf/03_window.el
+++ .emacs.d/conf/03_window.el
@@ -3,9 +3,12 @@
 ;;対応する括弧をハイライト
 (show-paren-mode 1)
 
+;; 現在行に色を付ける
+(global-hl-line-mode t)
+
 ;; window
 (tool-bar-mode -1)
-
+(scroll-bar-mode -1)
 
 ;;bell消す
 (setq ring-bell-function (lambda () ()))
