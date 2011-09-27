6517257db04dc098aa683daaeb5b94a2f96970f4
lapis25 <lapis25@gmail.com>
2011-09-27 17:06:56 +0900

    change install dir & add install comment

--- .emacs.d/conf/40_auto-complete.el
+++ .emacs.d/conf/40_auto-complete.el
@@ -1,3 +1,5 @@
+;; (auto-install-batch "auto-complete development version")
+
 (when (require 'auto-complete-config)
-  (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/install-lisp/ac-dict")
   (ac-config-default))
