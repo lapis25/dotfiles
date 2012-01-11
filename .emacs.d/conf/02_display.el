898e64dfa40cbb4eb48b9ca5bb0ab3f1e867f10a
lapis25 <kazuki.komori@mixi.co.jp>
2012-01-11 12:06:01 +0900

    add ansi-color setting

--- .emacs.d/conf/02_display.el
+++ .emacs.d/conf/02_display.el
@@ -3,3 +3,14 @@
 (global-font-lock-mode t)
 (require 'font-lock)
 
+;;ansi-color
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+;(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+(add-hook 'compilation-mode-hook 'ansi-color-for-comint-mode-on)
+(add-hook 'compilation-filter-hook
+          '(lambda ()
+             (let ((start-marker (make-marker))
+                   (end-marker (process-mark (get-buffer-process (current-buffer)))))
+               (set-marker start-marker (point-min))
+               (ansi-color-apply-on-region start-marker end-marker))))
