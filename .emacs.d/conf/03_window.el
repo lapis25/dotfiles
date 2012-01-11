6d89886ca4b7923fa7200d8a24cf67e0cc46bb07
lapis25 <kazuki.komori@mixi.co.jp>
2012-01-11 12:29:18 +0900

    いくつかまとめて

--- .emacs.d/conf/03_window.el
+++ .emacs.d/conf/03_window.el
@@ -3,12 +3,13 @@
 ;;対応する括弧をハイライト
 (show-paren-mode 1)
 
-;; 現在行に色を付ける
-(global-hl-line-mode t)
 
 ;; window
+(menu-bar-mode -1)
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
+
+(require 'color-theme)
+(color-theme-pok-wob)
 
 ;;bell消す
 (setq ring-bell-function (lambda () ()))
@@ -23,3 +24,17 @@
   (line-number-mode t)
   (column-number-mode t)
   (setq frame-title-format "%b"))
+
+;;; uniquify
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;;; popwin
+;;; (auto-install-from-url "https://raw.github.com/m2ym/popwin-el/v0.3/popwin.el")
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+(push '("*compilation*" :position right :width 0.5 :noselect t) popwin:special-display-config)
+
+;; 分割windowでも改行
+(setq truncate-partial-width-windows nil)
