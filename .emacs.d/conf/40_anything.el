60bc9c5abc608adb0765fcd74699b2670b8cca29
lapis25 <lapis25@gmail.com>
2013-11-04 00:04:01 +0900

    delete files
    Conflicts:
    	.emacs.d/conf/40_anything.el
    	.emacs.d/elisp/gtags.el

--- .emacs.d/conf/40_anything.el
+++
@@ -1,7 +0,0 @@
-;;; anything
-;; (auto-install-batch "anything")
-
-    (when (locate-library "elscreen")
-      (message "%s" (locate-library "elscreen")))
-(require 'anything-startup)
-
--- .emacs.d/conf/40_auto-install.el
+++
@@ -1,9 +0,0 @@
-;; auto-install
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/install-lisp/")
-(auto-install-update-emacswiki-package-name t)
-
-;;; install memo
-;; apel http://kanji.zinbun.kyoto-u.ac.jp/~tomo/elisp/APEL/
-;; make
-;; make install LISPDIR=~/.emacs.d/install-lisp VERSION_SPECIFIC_LISPDIR=~/.emacs.d/install-lisp
