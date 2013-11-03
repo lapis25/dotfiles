28ff0d3f4f2363b70fd53b1ae7af1ab18ad07178
lapis25 <lapis25@gmail.com>
2013-11-04 01:02:55 +0900

    update migemo

--- .emacs.d/conf/40_migemo.el
+++ .emacs.d/conf/40_migemo.el
@@ -1,9 +1,8 @@
 (when (and (executable-find "cmigemo")
            (require 'migemo nil t))
-  (setq migemo-command "cmigemo")
+   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs" "-i" "\a"))
-
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary (expand-file-name "~/local/share/migemo/utf-8/migemo-dict"))
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
   ;; cache setting
