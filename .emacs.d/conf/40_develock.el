f08f954a7bebaf8425530a4327581379796f371b
lapis25 <lapis25@gmail.com>
2013-11-04 21:18:54 +0900

    update develock

--- .emacs.d/conf/40_develock.el
+++ .emacs.d/conf/40_develock.el
@@ -1,3 +1,19 @@
-;; http://www.jpl.org/elips/develock.el.gz
-(require 'develock)
+; http://www.jpl.org/elips/develock.el.gz
+(require 'develock nil t)
 
+(if (featurep 'develock)
+    (progn
+      (setq develock-max-column-plist
+            (list 'java-mode  90
+                  'jde-mode   90
+                  'emacs-lisp-mode 90
+                  'lisp-interaction-mode 90
+                  'html-mode  nil
+                  'html-helper-mode nil
+                  'cperl-mode 90
+                  'perl-mode  90))
+      (let ((elem (copy-sequence (assq 'message-mode develock-keywords-alist))))
+        (setcar elem 'html-helper-mode)
+        (setq develock-keywords-alist
+              (cons elem (delq (assq 'html-helper-mode develock-keywords-alist)
+                               develock-keywords-alist))))
